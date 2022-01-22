7d0335fa3ef2f56e84fac16747a229137d24eb3f
Lucas Stecher <lucas.oliveira@coopersystem.com.br>
2022-01-22 17:00:56 -0300

    :sparkles: added graphql, creation and connection of database

--- src/index.js
+++ src/index.js
@@ -1,29 +1,46 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const cors = require('cors');
+const { ApolloServer } = require('apollo-server');
+require('dotenv').config();
+const database = require('./db/dbConnection');
+const Game = require('./db/model/game');
 
 const app = express();
 
 app.use(express.json(), cors());
 app.options('*', cors());
 
-const db = {
-    id: 1,
-    title: "Dark Souls",
-    picture: "https://upload.wikimedia.org/wikipedia/pt/7/7c/Dark_Souls_1_capa.png",
-    rating: 5,
-    studio: "FromSoftware",
-    release_date: "22-sep-2011",
-    story: "A spiritual successor to FromSoftware's Demon's Souls, the game is the second installment in the Souls series. Dark Souls takes place in the kingdom of Lordran, where players assume the role of a cursed undead character who begins a pilgrimage to discover the fate of their kind.",
-    genre: "souls-like"
-}
+// const server = new ApolloServer({
+//     typeDefs: fs.readFileSync(
+//         path.join(__dirname, 'game.graphql'),
+//         'utf8'
+//     ),
+//     resolvers
+// })
 
-app.get('/', (req, res) => {
-    return res.send('Welcome to GamePls API!');
-})
+// server
+//     .listen(process.env.PORT || 5000)
+//     .then(({ url }) => console.log(`Server is runnin on ${url}`)
+// );
 
-app.get('/dark-souls', (req, res) => {
-    return res.json(db);
-})
+// (async () => {
+//     try {
+//         const resultadoCreate = await Game.create({
+//             title: 'Red Dead Redemption 2',
+//             picture: 'https://image.api.playstation.com/cdn/UP1004/CUSA03041_00/Hpl5MtwQgOVF9vJqlfui6SDB5Jl4oBSq.png',
+//             rating: 5,
+//             studio: 'Rockstar Games',
+//             release_date: '26/10/2018',
+//             story: "Developed by the creators of Grand Theft Auto V and Red Dead Redemption, Red Dead Redemption 2 is an epic tale of life in America's unforgiving heartland. The game's vast and atmospheric world also provides the foundation for a brand new online multiplayer experience. America, 1899. The end of the Wild West era has begun. After a robbery goes badly wrong in the western town of Blackwater, Arthur Morgan and the Van der Linde gang are forced to flee. With federal agents and the best bounty hunters in the nation massing on their heels, the gang has to rob, steal and fight their way across the rugged heartland of America in order to survive. As deepening internal fissures threaten to tear the gang apart, Arthur must make a choice between his own ideals and loyalty to the gang that raised him.",
+//             genre: 'Ação-Aventura, Mundo Aberto',
+//         })
+//         console.log(resultadoCreate);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// })();
 
 app.listen(process.env.PORT || 5000);
-console.log('Server Running');
+console.log('Server is running');
